51e89f322cfaf574c340204b349d306b049853da
Matthew Tribby <mrt28@duke.edu>
2017-10-15 13:21:24 -0400

    basic firebase connection

--- app/src/main/java/mhealth/mvax/LoginActivity.java
+++ app/src/main/java/mhealth/mvax/LoginActivity.java
@@ -1,39 +1,28 @@
 package mhealth.mvax;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import java.util.ArrayList;
-import java.util.List;
 
-import static android.Manifest.permission.READ_CONTACTS;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * A login screen that offers login via email/password.
@@ -42,6 +31,8 @@ import static android.Manifest.permission.READ_CONTACTS;
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
     final String LOGIN_SIGNIN = "SIGN IN";
     final String LOGIN_REGISTER = "REGISTER";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
     /**
      * Id to identity READ_CONTACTS permission request.
      */
@@ -71,6 +62,24 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    //Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    //Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
 
@@ -113,6 +122,21 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     private void setAllText() {
--- app/src/main/java/mhealth/mvax/UserRegistrationActivity.java
+++ app/src/main/java/mhealth/mvax/UserRegistrationActivity.java
@@ -1,19 +1,22 @@
 package mhealth.mvax;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
-import java.util.*;
-import android.util.*;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.HashMap;
 
 public class UserRegistrationActivity extends AppCompatActivity {
     final String REG_Breg = "REGISTER";
     static UserRegistrationActivity checkLogin;
+    private FirebaseAuth mAuth;
+
 
     EditText newUserName, newUserEmail, newUserPassword;
     HashMap<String, String> registration = new HashMap<String, String>();
@@ -27,6 +30,8 @@ public class UserRegistrationActivity extends AppCompatActivity {
         newUserName = (EditText)findViewById(R.id.TFname);
         newUserEmail = (EditText)findViewById(R.id.TFemail);
         newUserPassword = (EditText)findViewById(R.id.TFpassword);
+
+        mAuth= FirebaseAuth.getInstance();
     }
 
     //Allow access to this activity through this method
@@ -40,7 +45,10 @@ public class UserRegistrationActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Puts email and password combination into dictionary
-                registration.put(newUserEmail.getText().toString(), newUserPassword.getText().toString());
+               // registration.put(newUserEmail.getText().toString(), newUserPassword.getText().toString());
+
+
+
                 Intent main = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(main);
             }
