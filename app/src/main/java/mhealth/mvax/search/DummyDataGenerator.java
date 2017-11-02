dd795a0388a1c6dabb451de9a6ae725356518c99
Robert Steilberg <rsteilberg@gmail.com>
2017-11-02 00:07:18 -0400

    add vaccine master data structure

--- app/src/main/java/mhealth/mvax/search/DummyDataGenerator.java
+++ app/src/main/java/mhealth/mvax/search/DummyDataGenerator.java
@@ -3,6 +3,8 @@ package mhealth.mvax.search;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 import mhealth.mvax.model.Sex;
 import mhealth.mvax.model.Record;
 import mhealth.mvax.model.Dose;
@@ -16,39 +18,15 @@ import mhealth.mvax.model.Vaccine;
 
 class DummyDataGenerator {
 
-    void generateDummyData() {
-        Vaccine hepatitis = new Vaccine("Hepatitis B");
-        Dose dose1 = new Dose("R.N.");
-        dose1.setDate(823237200000L);
-        hepatitis.addDose(dose1);
+    private DatabaseReference mDatabase;
 
+    DummyDataGenerator() {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+    }
 
-        Vaccine BCG = new Vaccine("BCG");
-        BCG.addDose(new Dose("1"));
+    void generateDummyPatientRecords() {
 
-        Vaccine polio = new Vaccine("Polio");
-        polio.addDose(new Dose("1", "VPI"));
-        polio.addDose(new Dose("2", "VOP"));
-        polio.addDose(new Dose("3", "VOP"));
-        polio.addDose(new Dose("Refuerzo", "VOP"));
-
-        Vaccine rotavirus = new Vaccine("Rotavirus");
-        rotavirus.addDose(new Dose("1"));
-        rotavirus.addDose(new Dose("2"));
-        rotavirus.addDose(new Dose("3"));
-        rotavirus.addDose(new Dose("4"));
-
-        Vaccine varicella = new Vaccine("Varicella");
-        varicella.addDose(new Dose("F.N."));
-
-        Vaccine syphilis = new Vaccine("Syphilis");
-        syphilis.addDose(new Dose("R.N.", "1"));
-        syphilis.addDose(new Dose("R.N.", "3"));
-        syphilis.addDose(new Dose("R.N.", "3"));
-        syphilis.addDose(new Dose("R.N.", "3"));
-
-        DatabaseReference db = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference patientRecords = db.child("patientRecords").push();
+        DatabaseReference patientRecords = mDatabase.child("patientRecords").push();
         Record rob = new Record(patientRecords.getKey());
         rob.setId("0123456789012");
         rob.setFirstName("Robert");
@@ -69,33 +47,70 @@ class DummyDataGenerator {
         rob.setParentAddress("9014 Tarrytown Drive, Richmond, VA 23229");
         rob.setParentPhone("8046904814");
 
-        rob.addVaccine(hepatitis);
-        rob.addVaccine(BCG);
-        rob.addVaccine(polio);
-        rob.addVaccine(rotavirus);
-        rob.addVaccine(varicella);
-        rob.addVaccine(syphilis);
         patientRecords.setValue(rob);
 
-//        patientRecords = _database.child("patientRecords").push();
-//        Record alison = new Record(patientRecords.getKey(), "Alison", "Huang", Sex.FEMALE, 1428206400000l, "West Bay");
-//        alison.addVaccine(hepatitis);
-//        alison.addVaccine(BCG);
-//        alison.addVaccine(polio);
-//        alison.addVaccine(rotavirus);
-//        alison.addVaccine(varicella);
-//        alison.addVaccine(syphilis);
-//        patientRecords.setValue(alison);
-//
-//        patientRecords = _database.child("patientRecords").push();
-//        Record steven = new Record(patientRecords.getKey(), "Steven", "Yang", Sex.MALE, 1078635600000l, "Oakridge");
-//        steven.addVaccine(hepatitis);
-//        steven.addVaccine(BCG);
-//        steven.addVaccine(polio);
-//        steven.addVaccine(rotavirus);
-//        steven.addVaccine(varicella);
-//        steven.addVaccine(syphilis);
-//        patientRecords.setValue(steven);
+        Record muffin = new Record(patientRecords.getKey());
+        muffin.setId("5748392019232");
+        muffin.setFirstName("Muffin");
+        muffin.setMiddleName("Lee");
+        muffin.setLastName("Bob");
+        muffin.setSuffix("VI");
+        muffin.setSex(Sex.FEMALE);
+        muffin.setDOB(823485940200L);
+        muffin.setPlaceOfBirth("Atlanta, GA");
+        muffin.setCommunity("Roatán");
+        muffin.setParentFirstName("Gus");
+        muffin.setParentMiddleName("Bert");
+        muffin.setParentLastName("Gussy");
+        muffin.setParentSuffix("IV");
+        muffin.setParentSex(Sex.MALE);
+        muffin.setParentId("3950481745324");
+        muffin.setNumDependents(2);
+        muffin.setParentAddress("1 Muffin Ln, Atlanta, GA");
+        muffin.setParentPhone("3840185960");
+
+        patientRecords.setValue(muffin);
+        
+    }
+
+    void generateDummyVaccineMaster() {
+        ArrayList<Vaccine> vaccines = new ArrayList<>();
+
+        Vaccine hepatitis = new Vaccine("Hepatitis B");
+        Dose dose1 = new Dose("R.N.");
+        hepatitis.addDose(dose1);
+        vaccines.add(hepatitis);
+
+        Vaccine BCG = new Vaccine("BCG");
+        BCG.addDose(new Dose("1"));
+        vaccines.add(BCG);
+
+        Vaccine polio = new Vaccine("Polio");
+        polio.addDose(new Dose("1", "VPI"));
+        polio.addDose(new Dose("2", "VOP"));
+        polio.addDose(new Dose("3", "VOP"));
+        polio.addDose(new Dose("Refuerzo", "VOP"));
+        vaccines.add(polio);
+
+        Vaccine rotavirus = new Vaccine("Rotavirus");
+        rotavirus.addDose(new Dose("1"));
+        rotavirus.addDose(new Dose("2"));
+        rotavirus.addDose(new Dose("3"));
+        rotavirus.addDose(new Dose("4"));
+        vaccines.add(rotavirus);
+
+        Vaccine varicella = new Vaccine("Varicella");
+        varicella.addDose(new Dose("F.N."));
+        vaccines.add(varicella);
+
+        Vaccine yellow = new Vaccine("Yellow Fever");
+        yellow.addDose(new Dose("R.N.", "1"));
+        yellow.addDose(new Dose("R.N.", "3"));
+        yellow.addDose(new Dose("R.N.", "3"));
+        yellow.addDose(new Dose("R.N.", "3"));
+        vaccines.add(yellow);
+
+        mDatabase.child("vaccineMaster").setValue(vaccines);
     }
 
 }
