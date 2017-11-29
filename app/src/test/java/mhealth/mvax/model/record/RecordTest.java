4f26f0869cc479356ce9164cd2394dfcf0ac4c13
Robert Steilberg <rsteilberg@gmail.com>
2017-11-28 19:41:03 -0500

    finish model tests

--- app/src/test/java/mhealth/mvax/model/record/RecordTest.java
+++ app/src/test/java/mhealth/mvax/model/record/RecordTest.java
@@ -1,63 +1,60 @@
 package mhealth.mvax.model.record;
 
-import org.junit.Before;
 import org.junit.Test;
-
-import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.ArrayList;
 
-import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.*;
+
+// https://github.com/ravidsrk/android-testing-guide#espresso
+// view coverage
+// https://developer.android.com/studio/test/index.html
 
 /**
  * @author Robert Steilberg
  */
 public class RecordTest {
 
-    @Before
-    public void setup() {
-    }
-
-
     @Test
-    public void testVaccineListSort() {
-        Vaccine a = new Vaccine("1", "a");
-        Vaccine f = new Vaccine("2", "f");
-
-        assertThat("Vaccines are not compared according to name",
-                a.compareTo(f),
-                lessThan(0));
+    public void vaccineListSort() throws Exception {
+        Vaccine a = new Vaccine("foo", "Hepatitis");
+        Vaccine b = new Vaccine("bar", "Rotavirus");
 
         ArrayList<Vaccine> vaccines = new ArrayList<>();
-        vaccines.add(f);
+        // record will be initialized with Vaccine array ["Rotavirus", "Hepatitis"]
         vaccines.add(a);
+        vaccines.add(b);
 
-        // record initialized with Vaccine array ["b", "a"]
-        Record record = new Record("1", vaccines);
+        Record record = new Record("foo", vaccines);
         String firstVaccineName = record.getVaccines().get(0).getName();
 
-        assertEquals("Initial record vaccine list not sorted by vaccine name", "a", firstVaccineName);
+        assertEquals("record's vaccine list not sorted by vaccine name after initialization",
+                "Hepatitis",
+                firstVaccineName);
 
         record.setVaccines(vaccines);
 
-        assertEquals("Subsequent set record vaccine list not sorted by vaccine name", "a", firstVaccineName);
+        assertEquals("record's vaccine list not sorted by vaccine name after setter",
+                "Hepatitis",
+                firstVaccineName);
     }
 
     @Test
-    public void testFullNameGetter() {
+    public void fullNameGetter() throws Exception {
         Record record = new Record();
 
-        record.setFirstName("Robert");
-        record.setLastName("Steilberg");
+        record.setFirstName("Foo");
+        record.setLastName("Bar");
 
-        assertEquals("Full name not computed correctly for first and last", "Steilberg, Robert", record.getFullName());
+        assertEquals("full name not computed correctly for first and last",
+                "Bar, Foo",
+                record.getFullName());
 
-        record.setMiddleName("Hays");
+        record.setMiddleName("Baz");
 
-        assertEquals("Full name not computed correctly for first, middle, and last", "Steilberg, Robert Hays", record.getFullName());
-
-
+        assertEquals("full name not computed correctly for first, middle, and last",
+                "Bar, Foo Baz",
+                record.getFullName());
     }
 
 }
--- app/src/test/java/mhealth/mvax/model/record/VaccineTest.java
+++ app/src/test/java/mhealth/mvax/model/record/VaccineTest.java
@@ -1,25 +1,50 @@
 package mhealth.mvax.model.record;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.*;
 
 /**
  * @author Robert Steilberg
  */
-class VaccineTest {
-    @org.junit.jupiter.api.Test
-    void getDoses() {
+public class VaccineTest {
+
+    @Test
+    public void addDose() throws Exception {
+        Vaccine vaccine = new Vaccine("foo", "Hepatitis");
+
+        assertEquals("new vaccine not initialized with empty dose list",
+                0,
+                vaccine.getDoses().size());
+
+        Dose dose = new Dose("foo", "bar");
+        vaccine.addDose(dose);
+
+        assertEquals("new doses not properly added to Vaccine's dose list",
+                1,
+                vaccine.getDoses().size());
     }
 
-    @org.junit.jupiter.api.Test
-    void setDoses() {
-    }
+    @Test
+    public void compareTo() throws Exception {
+        Vaccine a = new Vaccine("foo", "Rotavirus");
+        Vaccine b = new Vaccine("bar", "Rotavirus");
+        Vaccine c = new Vaccine("baz", "Yellow Fever");
 
-    @org.junit.jupiter.api.Test
-    void addDose() {
-    }
+        assertThat("vaccines not sorted by ascending name",
+                a.compareTo(c),
+                lessThan(0));
 
-    @org.junit.jupiter.api.Test
-    void compareTo() {
+        assertThat("vaccines not sorted by ascending name",
+                c.compareTo(a),
+                greaterThan(0));
+
+        assertEquals("vaccines with same name not sorted together",
+                0,
+                a.compareTo(b));
     }
 
 }
