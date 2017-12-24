458a422edc53bcd99e0d6109e61f82fe03262305
Robert Steilberg <rsteilberg@gmail.com>
2017-12-24 01:50:30 -0500

    create immunization package

--- app/src/main/java/mhealth/mvax/model/immunization/Vaccination.java
+++ app/src/main/java/mhealth/mvax/model/immunization/Vaccination.java
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public
 License along with mVax; see the file LICENSE. If not, see
 <http://www.gnu.org/licenses/>.
 */
-package mhealth.mvax.model.record;
+package mhealth.mvax.model.immunization;
 
 import java.io.Serializable;
 
