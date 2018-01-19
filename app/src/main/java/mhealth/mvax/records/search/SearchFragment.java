103881f782961a779ebe8404a1312a782bee5ac3
Robert Steilberg <rsteilberg@gmail.com>
2018-01-18 22:13:03 -0500

    implement basic Firebase querying

--- app/src/main/java/mhealth/mvax/records/search/SearchFragment.java
+++ app/src/main/java/mhealth/mvax/records/search/SearchFragment.java
@@ -22,6 +22,8 @@ package mhealth.mvax.records.search;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,9 +40,12 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import mhealth.mvax.R;
 import mhealth.mvax.model.record.Patient;
@@ -67,6 +72,8 @@ public class SearchFragment extends Fragment {
     private DatabaseReference mPatientRef;
     private ChildEventListener mPatientListener;
 
+    private View mView;
+
     //================================================================================
     // Static methods
     //================================================================================
@@ -81,28 +88,108 @@ public class SearchFragment extends Fragment {
 
     @Override
     public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.fragment_search, container, false);
+        mView = inflater.inflate(R.layout.fragment_search, container, false);
         mPatients = new HashMap<>();
-        mSearchResultAdapter = new SearchResultAdapter(view.getContext(), mPatients.values());
+        mSearchResultAdapter = new SearchResultAdapter(mView.getContext(), mPatients.values());
 
-        initDatabase();
-        renderNewRecordButton(view, inflater);
-        renderFilterSpinner(view);
-        initRecordFilters(view);
-        renderListView(view);
+//        initDatabase();
+        renderNewRecordButton(mView, inflater);
+//        renderFilterSpinner(view);
 
-        return view;
+
+        initSearchBar();
+
+
+        renderListView(mView);
+
+        return mView;
     }
 
     @Override
     public void onDestroyView() {
-        mPatientRef.removeEventListener(mPatientListener);
+//        mPatientRef.removeEventListener(mPatientListener);
         super.onDestroyView();
     }
 
     //================================================================================
     // Private methods
     //================================================================================
+
+
+    private Timer searchTimer = new Timer();
+
+    private void initSearchBar() {
+        final EditText searchBar = mView.findViewById(R.id.search_bar);
+
+        searchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(final CharSequence charSequence, int i, int i1, int i2) {
+                try {
+                    searchTimer.cancel();
+                } catch (IllegalStateException ignored) {
+                }
+                searchTimer = new Timer();
+                searchTimer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        search(charSequence.toString());
+                    }
+                }, 1000);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+    private void search(String query) {
+        // define database ref
+        final String dataTable = getResources().getString(R.string.dataTable);
+        final String patientTable = getResources().getString(R.string.patientTable);
+        Query q = FirebaseDatabase.getInstance().getReference()
+                .child(dataTable)
+                .child(patientTable)
+                .orderByChild("fullName")
+                .equalTo(query);
+
+
+        q.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                final Patient patient = dataSnapshot.getValue(Patient.class);
+                if (patient != null) {
+                    mPatients.put(patient.getDatabaseKey(), patient);
+                    mSearchResultAdapter.refresh(mPatients.values());
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
 
     /**
      * Initializes the Firebase connection and sets up data listeners
@@ -169,33 +256,32 @@ public class SearchFragment extends Fragment {
                 .commit();
     }
 
-    private void renderFilterSpinner(View view) {
-        // TODO replace all of this with Firebase queries
-        final Spinner spinner = view.findViewById(R.id.search_filter_spinner);
-        ArrayAdapter<CharSequence> filterAdapter = ArrayAdapter.createFromResource(view.getContext(),
-                R.array.filter_spinner_array, android.R.layout.simple_spinner_item);
-        filterAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(filterAdapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
-                if (pos != 0) {
-                    mSearchFilter.setFilter(spinner.getItemAtPosition(pos).toString());
-                }
-            }
+//    private void renderFilterSpinner(View view) {
+//        // TODO replace all of this with Firebase queries
+//        final Spinner spinner = view.findViewById(R.id.search_filter_spinner);
+//        ArrayAdapter<CharSequence> filterAdapter = ArrayAdapter.createFromResource(view.getContext(),
+//                R.array.filter_spinner_array, android.R.layout.simple_spinner_item);
+//        filterAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner.setAdapter(filterAdapter);
+//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+//                if (pos != 0) {
+//                    mSearchFilter.setFilter(spinner.getItemAtPosition(pos).toString());
+//                }
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> adapterView) {
+//            }
+//        });
+//    }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-            }
-        });
-    }
-
-    private void initRecordFilters(View view) {
-        // TODO replace all of this with Firebase queries
-        final EditText searchBar = view.findViewById(R.id.search_bar);
-        mSearchFilter = new SearchFilter(mPatients, mSearchResultAdapter, searchBar);
-        mSearchFilter.addFilters();
-    }
+//    private void initRecordFilters(View view) {
+//        final EditText searchBar = view.findViewById(R.id.search_bar);
+//        mSearchFilter = new SearchFilter(mPatients, mSearchResultAdapter, searchBar);
+//        mSearchFilter.addFilters();
+//    }
 
     private void renderListView(View view) {
         final ListView patientListView = view.findViewById(R.id.record_list_view);
@@ -213,7 +299,6 @@ public class SearchFragment extends Fragment {
             }
         });
     }
-
 
 
 }
