b2a3d0f389dae298af50073a65264557a0a8fd18
Steven Yang <sgyang9@gmail.com>
2018-04-03 01:50:01 -0400

    added calendar to alerts

--- app/src/main/java/mhealth/mvax/alerts/AlertsFragment.java
+++ app/src/main/java/mhealth/mvax/alerts/AlertsFragment.java
@@ -24,6 +24,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.CalendarView;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
 import mhealth.mvax.R;
@@ -56,141 +57,192 @@ public class AlertsFragment extends Fragment {
     ChildEventListener mPatientListener;
 
 
-    public static AlertsFragment newInstance() {
-//        AlertsFragment fragment = new AlertsFragment();
-//        return fragment;
-        return new AlertsFragment();
-    }
+    //Calendar Integration
+
+    CalendarView calendar;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        
+        //initializes the calendarview
+        initializeCalendar();
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_overdue, null);
-        ExpandableListView elv = (ExpandableListView) v.findViewById(R.id.list);
-        elv.setAdapter(new SavedTabsListAdapter());
-        return v;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        return view;
     }
 
+    public void initializeCalendar() {
+        calendar = (CalendarView) findViewById(R.id.calendar);
 
-    private boolean initDatabase(){
-        mAuth = FirebaseAuth.getInstance();
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        // sets whether to show the week number.
+        calendar.setShowWeekNumber(false);
 
-        String masterTable = getResources().getString(R.string.dataTable);
-        //get overdue table
-        String vaccineTable = getResources().getString(R.string.vaccineTable);
+        // sets the first day of week according to Calendar.
+        // here we set Monday as the first day of the Calendar
+        calendar.setFirstDayOfWeek(2);
 
-        mPatientListener = new ChildEventListener() {
+        //The background color for the selected week.
+        calendar.setSelectedWeekBackgroundColor(getResources().getColor(R.color.green));
+
+        //sets the color for the dates of an unfocused month.
+        calendar.setUnfocusedMonthDateColor(getResources().getColor(R.color.transparent));
+
+        //sets the color for the separator line between weeks.
+        calendar.setWeekSeparatorLineColor(getResources().getColor(R.color.transparent));
+
+        //sets the color for the vertical bar shown at the beginning and at the end of the selected date.
+        calendar.setSelectedDateVerticalBar(R.color.darkgreen);
+
+        //sets the listener to be notified upon selected date change.
+        calendar.setOnDateChangeListener(new OnDateChangeListener() {
+            //show the selected date as a toast
             @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-
+            public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
+                Toast.makeText(getApplicationContext(), day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
             }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-
-        };
-        return true;
+        });
     }
 
 
-    public class SavedTabsListAdapter extends BaseExpandableListAdapter {
 
-
-        private String[] groups = { getResources().getString(R.string.high_priority), getResources().getString(R.string.medium_priority), getResources().getString(R.string.low_priority)};
-
-        private String[][] children = {
-                { "Steilberg, Robert Hays II", "Bob, Muffin Lee IV"},
-                { "Bob, Muffin Lee IV"},
-                { "Steilberg, Robert Hays II"}
-        };
-
-        @Override
-        public int getGroupCount() {
-            return groups.length;
-        }
-
-        @Override
-        public int getChildrenCount(int i) {
-            return children[i].length;
-        }
-
-        @Override
-        public Object getGroup(int i) {
-            return groups[i];
-        }
-
-        @Override
-        public Object getChild(int i, int i1) {
-            return children[i][i1];
-        }
-
-        @Override
-        public long getGroupId(int i) {
-            return i;
-        }
-
-        @Override
-        public long getChildId(int i, int i1) {
-            return i1;
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
-        @Override
-        public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
-            TextView textView = new TextView(AlertsFragment.this.getActivity());
-            textView.setText(getGroup(i).toString());
-            textView.setTextSize(35);
-            textView.setPadding(100,0,0,0);
-            return textView;
-        }
-
-        @Override
-        public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
-            TextView textView = new TextView(AlertsFragment.this.getActivity());
-            //Temporary PlaceHolder for patient contact info - Demo purpose
-            if(getChild(i, i1).toString().equals("Steilberg, Robert Hays II")){
-                textView.setText(getChild(i, i1).toString() + "\nPhone Number: 8046904814" + "\nCommunity: Alspaugh" + "\nAddress: 9014 Tarrytown Drive, Richmind, VA 23229");
-            }
-            else{
-                textView.setText(getChild(i, i1).toString() + "\nPhone Number: 3840185960" + "\nCommunity: Roatan" + "\nAddress: 1 Muffin Ln, Atlanta, GA");
-            }
-            //textView.setText(getChild(i, i1).toString() + " Phone Number: " + "8046904814");
-            textView.setTextSize(25);
-            textView.setPadding(100,0,0,0);
-            return textView;
-        }
-
-        @Override
-        public boolean isChildSelectable(int i, int i1) {
-            return true;
-        }
-
-    }
+//    public static AlertsFragment newInstance() {
+////        AlertsFragment fragment = new AlertsFragment();
+////        return fragment;
+//        return new AlertsFragment();
+//    }
+//
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        View v = inflater.inflate(R.layout.fragment_overdue, null);
+//        ExpandableListView elv = (ExpandableListView) v.findViewById(R.id.list);
+//        elv.setAdapter(new SavedTabsListAdapter());
+//        return v;
+//    }
+//
+//
+//    private boolean initDatabase(){
+//        mAuth = FirebaseAuth.getInstance();
+//        mDatabase = FirebaseDatabase.getInstance().getReference();
+//
+//        String masterTable = getResources().getString(R.string.dataTable);
+//        //get overdue table
+//        String vaccineTable = getResources().getString(R.string.vaccineTable);
+//
+//        mPatientListener = new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onChildRemoved(DataSnapshot dataSnapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//
+//        };
+//        return true;
+//    }
+//
+//
+//    public class SavedTabsListAdapter extends BaseExpandableListAdapter {
+//
+//
+//        private String[] groups = { getResources().getString(R.string.high_priority), getResources().getString(R.string.medium_priority), getResources().getString(R.string.low_priority)};
+//
+//        private String[][] children = {
+//                { "Steilberg, Robert Hays II", "Bob, Muffin Lee IV"},
+//                { "Bob, Muffin Lee IV"},
+//                { "Steilberg, Robert Hays II"}
+//        };
+//
+//        @Override
+//        public int getGroupCount() {
+//            return groups.length;
+//        }
+//
+//        @Override
+//        public int getChildrenCount(int i) {
+//            return children[i].length;
+//        }
+//
+//        @Override
+//        public Object getGroup(int i) {
+//            return groups[i];
+//        }
+//
+//        @Override
+//        public Object getChild(int i, int i1) {
+//            return children[i][i1];
+//        }
+//
+//        @Override
+//        public long getGroupId(int i) {
+//            return i;
+//        }
+//
+//        @Override
+//        public long getChildId(int i, int i1) {
+//            return i1;
+//        }
+//
+//        @Override
+//        public boolean hasStableIds() {
+//            return true;
+//        }
+//
+//        @Override
+//        public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+//            TextView textView = new TextView(AlertsFragment.this.getActivity());
+//            textView.setText(getGroup(i).toString());
+//            textView.setTextSize(35);
+//            textView.setPadding(100,0,0,0);
+//            return textView;
+//        }
+//
+//        @Override
+//        public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+//            TextView textView = new TextView(AlertsFragment.this.getActivity());
+//            //Temporary PlaceHolder for patient contact info - Demo purpose
+//            if(getChild(i, i1).toString().equals("Steilberg, Robert Hays II")){
+//                textView.setText(getChild(i, i1).toString() + "\nPhone Number: 8046904814" + "\nCommunity: Alspaugh" + "\nAddress: 9014 Tarrytown Drive, Richmind, VA 23229");
+//            }
+//            else{
+//                textView.setText(getChild(i, i1).toString() + "\nPhone Number: 3840185960" + "\nCommunity: Roatan" + "\nAddress: 1 Muffin Ln, Atlanta, GA");
+//            }
+//            //textView.setText(getChild(i, i1).toString() + " Phone Number: " + "8046904814");
+//            textView.setTextSize(25);
+//            textView.setPadding(100,0,0,0);
+//            return textView;
+//        }
+//
+//        @Override
+//        public boolean isChildSelectable(int i, int i1) {
+//            return true;
+//        }
+//
+//    }
 
 }
