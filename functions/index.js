78aec5474fc939835bb14617a93a979cbb7f00a0
Robert Steilberg <rsteilberg@gmail.com>
2018-04-17 16:58:52 -0400

    refactor auth workflow

--- functions/index.js
+++ functions/index.js
@@ -36,7 +36,7 @@ exports.activateAccount = functions.https.onCall((data, context) => {
     disabled: false
   })
   .then(function(userRecord) {
-    sendAccountActivatedEmail(data.email, data.subject, data.body);
+//    sendAccountActivatedEmail(data.email, data.subject, data.body);
     console.log("user " + data.uid + " activated")
   }).catch(function(error) {
     throw new functions.https.HttpsError('error', error);
@@ -53,29 +53,29 @@ exports.deleteAccount = functions.https.onCall((data, context) => {
 });
 
 
-const nodemailer = require('nodemailer');
-const gmailEmail = functions.config().gmail.email;
-const gmailPassword = functions.config().gmail.password;
-const mailTransport = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: gmailEmail,
-    pass: gmailPassword,
-  },
-});
-
-function sendAccountActivatedEmail(email, subject, body) {
-  const mailOptions = {
-    from: `mVax <mvaxapp@gmail.com>`,
-    to: email,
-  };
-
-  mailOptions.subject = subject;
-  mailOptions.text = body;
-  return mailTransport.sendMail(mailOptions).then(() => {
-    return console.log('account activated email sent to ', email);
-  });
-}
+//const nodemailer = require('nodemailer');
+//const gmailEmail = functions.config().gmail.email;
+//const gmailPassword = functions.config().gmail.password;
+//const mailTransport = nodemailer.createTransport({
+//  service: 'gmail',
+//  auth: {
+//    user: gmailEmail,
+//    pass: gmailPassword,
+//  },
+//});
+//
+//function sendAccountActivatedEmail(email, subject, body) {
+//  const mailOptions = {
+//    from: `mVax <mvaxapp@gmail.com>`,
+//    to: email,
+//  };
+//
+//  mailOptions.subject = subject;
+//  mailOptions.text = body;
+//  return mailTransport.sendMail(mailOptions).then(() => {
+//    return console.log('account activated email sent to ', email);
+//  });
+//}
 
 
 // ALGOLIA SEARCH
